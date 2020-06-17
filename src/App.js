b42c117aac09cbc36683a2c4d1e75f6928b65ffa
sooman-hub <soomansamad@gmail.com>
2020-06-17 23:09:52 +0500

    Remove unused logo image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
     return ( <
         div className = "App" >
         <
-        h1 > Hello World < /h1> <
-        /div>
+        h1 > Hello World < /h1> < /
+        div >
     );
 }
 
